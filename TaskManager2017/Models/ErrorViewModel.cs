2c56a8c192fa9788522f8df6f86f479c05bdeed6
Laura Clover <laura.clover3@gmail.com>
2018-05-03 19:13:31 -0500

    Add project files.

---
+++ TaskManager2017/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskManager2017.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
